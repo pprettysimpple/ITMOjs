62fe9db26f65e219a10fe5a104264e265c58e6a8
pprettysimpple <andrianov.kirill2010@yandex.ru>
2020-04-17 12:55:27 +0300

    functional last fix

--- functionalExpression.js
+++ functionalExpression.js
@@ -1,7 +1,7 @@
 "use strict";
 
 // operations
-const cnst = (c) => (() => c);
+const cnst = c => () => c;
 const getI = i => (...args) => args[i];
 const VARIABLES = {
 	"x": getI(0),
@@ -13,37 +13,39 @@ const two = cnst(2);
 const CONSTANTS = {
   "one": one,
   "two": two
-}
+};
+
 const variable = (s) => VARIABLES[s];
-const getOperation = (operation) => (...operands) => (...args) => operation(...operands.map((exp) => exp(...args)));
+const getOperation = (operation) => {
+	let cur = (...operands) => (...args) => operation(...operands.map((exp) => exp(...args)));
+	cur.size = operation.length;
+	return cur;
+}
 const add = getOperation((a, b) => a + b);
 const subtract = getOperation((a, b) => a - b);
 const multiply = getOperation((a, b) => a * b);
 const divide = getOperation((a, b) => a / b);
 const negate = getOperation(a => -a);
+const abs = getOperation(Math.abs);
+const iff = getOperation((a, b, c) => a >= 0 ? b : c);
 
 // parser
-const opInfo = function(type, size) {
-	this.type = type;
-	this.size = size;
-}
 const OPERATIONS = {
-	"+": new opInfo(add, 2),
-	"-": new opInfo(subtract, 2),
-	"*": new opInfo(multiply, 2),
-	"/": new opInfo(divide, 2),
-	"negate": new opInfo(negate, 1)
-}
-const getTokens = (s) => s.split(' ').filter((token) => (token.length > 0));
-const parse = (input) => {
-	return getTokens(input).reduce(
+	"+": add,
+	"-": subtract,
+	"*": multiply,
+	"/": divide,
+	"negate": negate,
+	"iff": iff,
+	"abs": abs
+};
+function parse(input) {
+	return input.split(' ').filter(token => token.length > 0).reduce(
 		(stack, token) => {
 			if (token in OPERATIONS) {
 				let op = OPERATIONS[token];
-				let suffix = stack.splice(
-					stack.length - op.size
-				);
-				stack.push(op.type(...suffix));
+				let suffix = stack.splice(stack.length - op.size);
+				stack.push(op(...suffix));
 			} else if (token in VARIABLES) {
 				stack.push(VARIABLES[token]);
 			} else if (token in CONSTANTS) {
@@ -57,3 +59,6 @@ const parse = (input) => {
 		[]
 	).pop();
 }
+
+// let expr = abs(add(subtract(cnst(1), cnst(2)), variable('x')));
+// console.log(expr(-7, 2, 3));
